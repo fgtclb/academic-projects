be1e68d73a601139fd755e7a723573fa6ca1fe79
Riad <riad@web-vision.de>
2023-09-20 11:17:22 +0200

    [TASK] Prepare Release 0.1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,11 +5,11 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Academic project pages for TYPO3 with specific structured data and typed sys_categories',
     'category' => 'fe',
     'state' => 'beta',
-    'version' => '0.1.1',
+    'version' => '0.1.2',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
-            'fgtclb_educational' => '*',
+            'category_types' => '^1.0',
         ],
         'conflicts' => [],
         'suggests' => [],
