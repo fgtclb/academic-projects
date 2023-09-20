a6ce8e619cb60927626ab4fdcf986ba44694d762
Riad <riad@web-vision.de>
2023-09-20 10:11:46 +0200

    [TASK] Prepare Release

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Academic Projects
+
+This extension adds an academic projects database to TYPO3.
+Records can be created, edit and displayed in the frontend.
+There is a list and a detail view for the frontend .
+
+**This extension is currently in beta state - please notice that there might be changes to the structure**
+
+## Installation
+
+```shell
+composer require fgtclb/academic-projects
+```
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 $EM_CONF[$_EXTKEY] = [
     'title' => '(FGTCLB) Academic Projects',
     'description' => 'Academic project pages for TYPO3 with specific structured data and typed sys_categories',
     'category' => 'fe',
     'state' => 'beta',
-    'version' => '1.0.0',
+    'version' => '0.1.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
