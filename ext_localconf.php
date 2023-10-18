0975d649926e516bce9eeacfc3aa8a52ec23612b
Philipp Halle <p.halle@web-vision.de>
2023-10-18 10:50:22 +0200

    [TASK] Add list plugin to new content element wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,11 @@
 <?php
 
 (static function (): void {
-    $projectDokType = \FGTCLB\AcademicProjects\Domain\Enumeration\Page::TYPE_EDUCATIONAL_PROJECT;
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '@import \'EXT:academic_projects/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig\''
+    );
+
+    $projectDokType = \FGTCLB\AcademicProjects\Domain\Enumeration\Page::TYPE_ACEDEMIC_PROJECT;
     // Allow backend users to drag and drop the new page type:
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
         sprintf(
