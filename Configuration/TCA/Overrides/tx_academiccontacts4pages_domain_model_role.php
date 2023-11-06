0ffb2a33046c21ffb48a7535fc5474afc8541295
Mark <markhouben91@gmail.com>
2023-11-06 16:49:05 +0100

    [TASK] Add restriction checks for extension field changes

--- Configuration/TCA/Overrides/tx_academiccontacts4pages_domain_model_role.php
+++ Configuration/TCA/Overrides/tx_academiccontacts4pages_domain_model_role.php
@@ -7,21 +7,25 @@ if (!defined('TYPO3')) {
 }
 
 (static function (): void {
+    // Field configuration only use with extension "academic_contacts4pages" and table
+    if (!ExtensionManagementUtility::isLoaded('academic_contacts4pages')) {
+        return;
+    }
+    if (!isset($GLOBALS['TCA']['tx_academiccontacts4pages_domain_model_role'])) {
+        return;
+    }
+
     $ll = function (string $langKey): string {
         return 'LLL:EXT:academic_projects/Resources/Private/Language/locallang_db.xlf:' . $langKey;
     };
 
-    $doktype = \FGTCLB\AcademicProjects\Domain\Enumeration\Page::TYPE_ACEDEMIC_PROJECT;
-
-    if (array_key_exists('tx_academiccontacts4pages_domain_model_role', $GLOBALS['TCA'])) {
-        ExtensionManagementUtility::addTcaSelectItem(
-            'tx_academiccontacts4pages_domain_model_role',
-            'doktypes',
-            [
-                $ll('pages.doktype.item.academic_projects'),
-                $doktype,
-                'actions-code-merge',
-            ]
-        );
-    }
+    ExtensionManagementUtility::addTcaSelectItem(
+        'tx_academiccontacts4pages_domain_model_role',
+        'doktypes',
+        [
+            $ll('pages.doktype.item.academic_projects'),
+            \FGTCLB\AcademicProjects\Domain\Enumeration\Page::TYPE_ACEDEMIC_PROJECT,
+            'actions-code-merge',
+        ]
+    );
 })();
