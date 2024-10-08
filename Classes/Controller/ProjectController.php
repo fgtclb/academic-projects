6e7d06e87763d7803b752d56fa6818fde077c8f8
Philipp Halle <p.halle@web-vision.de>
2024-10-08 14:25:25 +0200

    [TASK] Add factory for project demand

--- Classes/Controller/ProjectController.php
+++ Classes/Controller/ProjectController.php
@@ -4,51 +4,41 @@ declare(strict_types=1);
 
 namespace FGTCLB\AcademicProjects\Controller;
 
-use FGTCLB\AcademicProjects\Domain\Model\Dto\ProjectFilter;
 use FGTCLB\AcademicProjects\Domain\Repository\CategoryRepository;
 use FGTCLB\AcademicProjects\Domain\Repository\ProjectRepository;
+use FGTCLB\AcademicProjects\Factory\DemandFactory;
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class ProjectController extends ActionController
 {
     public function __construct(
         protected readonly ProjectRepository $projectRepository,
-        protected readonly CategoryRepository $categoryRepository
+        protected readonly CategoryRepository $categoryRepository,
+        protected readonly DemandFactory $demandFactory
     ) {
     }
 
-    public function listAction(?ProjectFilter $filter = null): ResponseInterface
+    /**
+     * @param array<string, mixed>|null $demand
+     * @return ResponseInterface
+     */
+    public function listAction(?array $demand = null): ResponseInterface
     {
-        $hideCompletedProjects = true;
+        $contentElementData = $this->configurationManager->getContentObject()->data;
 
-        if (!$filter) {
-            $filter = new ProjectFilter();
-
-            if ($this->settings['sorting']) {
-                $filter->setSorting($this->settings['sorting']);
-            }
-        }
-
-        if (isset($this->settings['hide_completed_projects'])) {
-            $hideCompletedProjects = (bool)$this->settings['hide_completed_projects'];
-        }
-
-        $projects = $this->projectRepository->findByFilter(
-            $hideCompletedProjects,
-            $filter,
-            $this->configurationManager->getContentObject()->data['pages'] !== ''
-                ? GeneralUtility::intExplode(',', $this->configurationManager->getContentObject()->data['pages'])
-                : [],
-            $this->configurationManager->getContentObject()->data['list_type'] === 'academicprojects_projectlistsingle' ? true : false
+        $demandObject = $this->demandFactory->createDemandObject(
+            $demand,
+            $this->settings,
+            $contentElementData
         );
 
+        $projects = $this->projectRepository->findByDemand($demandObject);
         $categories = $this->categoryRepository->findAllApplicable($projects);
 
         $assignedValues = [
             'projects' => $projects,
-            'filter' => $filter,
+            'demand' => $demandObject,
             'categories' => $categories,
         ];
 
