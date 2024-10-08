ae249a5de55c1319bb245103e8268f2e388a9964
Philipp Halle <p.halle@web-vision.de>
2024-10-08 14:28:23 +0200

    [TASK] Add classes to Services.php for DI

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -2,6 +2,10 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use FGTCLB\AcademicProjects\Collection\FilterCollection;
+use FGTCLB\AcademicProjects\Domain\Repository\CategoryRepository;
+use FGTCLB\AcademicProjects\Domain\Repository\ProjectRepository;
+
 return function (ContainerConfigurator $containerConfigurator) {
     $services = $containerConfigurator
         ->services()
@@ -12,4 +16,16 @@ return function (ContainerConfigurator $containerConfigurator) {
     $services
         ->load('FGTCLB\\AcademicProjects\\', '../Classes/')
         ->exclude('../Classes/Domain/Model/');
+
+    $services
+        ->set(FilterCollection::class)
+        ->public();
+
+    $services
+        ->set(CategoryRepository::class)
+        ->public();
+
+    $services
+        ->set(ProjectRepository::class)
+        ->public();
 };
