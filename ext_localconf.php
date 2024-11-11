a670304ee9aa929bdc03a568f4ad97e086bd861f
Philipp Halle <p.halle@web-vision.de>
2024-11-11 12:48:33 +0100

    [TASK] Add page.tsconfig manually only for TYPO3 < v12

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,10 +6,15 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 (static function (): void {
-    ExtensionManagementUtility::addPageTSConfig('
-        @import \'EXT:academic_projects/Configuration/TsConfig/Page/Mod/Wizards/NewContentElement.tsconfig\'
-        @import \'EXT:academic_projects/Configuration/TsConfig/Page/Tceform/Pages.tsconfig\'
-    ');
+    $versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+
+    // Starting with TYPO3 v12.0 Configuration/page.tsconfig in an Extension is automatically loaded during build time
+    // @see https://docs.typo3.org/m/typo3/reference-tsconfig/12.4/en-us/UsingSetting/PageTSconfig.html#pagesettingdefaultpagetsconfig
+    if ($versionInformation->getMajorVersion() < 12) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+            @import \'EXT:academic_programs/Configuration/page.tsconfig\'
+        ');
+    }
 
     $projectDokType = PageTypes::TYPE_ACEDEMIC_PROJECT;
     // Allow backend users to drag and drop the new page type:
