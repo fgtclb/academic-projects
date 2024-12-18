e800a6b21d4837cf681a4e53b473e2d4156919e3
Philipp Halle <p.halle@web-vision.de>
2024-12-18 14:47:09 +0100

    [TASK] Copy changes on select view helpers from academic_programs

--- Classes/ViewHelpers/Form/AbstractSelectViewHelper.php
+++ Classes/ViewHelpers/Form/AbstractSelectViewHelper.php
@@ -4,17 +4,69 @@ declare(strict_types=1);
 
 namespace FGTCLB\AcademicProjects\ViewHelpers\Form;
 
-class AbstractSelectViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\SelectViewHelper
+use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
+use TYPO3\CMS\Fluid\ViewHelpers\Form\AbstractFormFieldViewHelper;
+
+class AbstractSelectViewHelper extends AbstractFormFieldViewHelper
 {
+    /**
+     * @var string
+     */
+    protected $tagName = 'select';
+
+    /**
+     * @var string
+     */
+    protected $selectedValue;
+
     public function initializeArguments(): void
     {
         parent::initializeArguments();
-        $this->registerArgument('renderOptions', 'bool', 'If true, options will be rendered. Otherwise an "options" variable is created for custom rendering in the template.', false, true);
+
+        $arguments = [
+            'options' => [
+                'type' => 'array',
+                'defaultValue' => [],
+                'description' => 'Associative array with internal IDs as key, and the values are displayed in the select box. Can be combined with or replaced by child f:form.select.* nodes.',
+            ],
+            'optionsAfterContent' => [
+                'type' => 'boolean',
+                'defaultValue' => false,
+                'description' => 'If true, places auto-generated option tags after those rendered in the tag content. If false, automatic options come first.',
+            ],
+            'sortByOptionLabel' => [
+                'type' => 'boolean',
+                'defaultValue' => false,
+                'description' => 'If true, List will be sorted by label.',
+            ],
+            'errorClass' => [
+                'type' => 'string',
+                'defaultValue' => 'f3-form-error',
+                'description' => 'CSS class to set if there are errors for this ViewHelper',
+            ],
+            'prependOptionLabel' => [
+                'type' => 'string',
+                'description' => 'If specified, will provide an option at first position with the specified label.',
+            ],
+            'prependOptionValue' => [
+                'type' => 'string',
+                'description' => 'If specified, will provide an option at first position with the specified value.',
+            ],
+            'required' => [
+                'type' => 'boolean',
+                'defaultValue' => false,
+                'description' => 'If set no empty value is allowed.',
+            ],
+            'renderOptions' => [
+                'type' => 'bool',
+                'defaultValue' => true,
+                'description' => 'If true, options will be rendered. Otherwise an "options" variable is created for custom rendering in the template.',
+            ],
+        ];
+
+        $this->registerArguments($arguments);
     }
 
-    /**
-     * @return string
-     */
     public function render(): string
     {
         if (isset($this->arguments['required']) && $this->arguments['required']) {
@@ -22,15 +74,13 @@ class AbstractSelectViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\SelectV
         }
 
         $name = $this->getName();
-        if (isset($this->arguments['multiple']) && $this->arguments['multiple']) {
-            $this->tag->addAttribute('multiple', 'multiple');
-            $name .= '[]';
-        }
         $this->tag->addAttribute('name', $name);
 
         $options = $this->getOptions();
 
-        if (isset($this->arguments['renderOptions']) && (bool)$this->arguments['renderOptions'] === false) {
+        if (isset($this->arguments['renderOptions'])
+            && (bool)$this->arguments['renderOptions'] === false
+        ) {
             $this->renderingContext->getVariableProvider()->add('options', $options);
         }
 
@@ -43,33 +93,12 @@ class AbstractSelectViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\SelectV
         // Register field name for token generation.
         $this->registerFieldNameForFormTokenGeneration($name);
 
-        // In case it is a multi-select, we need to register the field name
-        // as often as there are elements in the box
-        if (isset($this->arguments['multiple']) && $this->arguments['multiple']) {
-            $content .= $this->renderHiddenFieldForEmptyValue();
-
-            // Register the field name additional times as required by the total number of
-            // options. Since we already registered it once above, we start the counter at 1
-            // instead of 0.
-            $optionsCount = count($options);
-            for ($i = 1; $i < $optionsCount; $i++) {
-                $this->registerFieldNameForFormTokenGeneration($name);
-            }
-
-            // Save the parent field name so that any child f:form.select.option
-            // tag will know to call registerFieldNameForFormTokenGeneration
-            // this is the reason why "self::class" is used instead of static::class (no LSB)
-            $viewHelperVariableContainer->addOrUpdate(
-                self::class,
-                'registerFieldNameForFormTokenGeneration',
-                $name
-            );
-        }
-
         $prependContent = $this->renderPrependOptionTag();
 
         $tagContent = '';
-        if (isset($this->arguments['renderOptions']) && (bool)$this->arguments['renderOptions'] === true) {
+        if (isset($this->arguments['renderOptions'])
+            && (bool)$this->arguments['renderOptions'] === true
+        ) {
             $tagContent = $this->renderOptionTags($options);
         }
 
@@ -95,5 +124,84 @@ class AbstractSelectViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Form\SelectV
         $content .= $this->tag->render();
 
         return $content;
+    }
+
+    protected function renderPrependOptionTag(): string
+    {
+        $output = '';
+        if ($this->hasArgument('prependOptionLabel')) {
+            $value = $this->hasArgument('prependOptionValue') ? $this->arguments['prependOptionValue'] : '';
+            $label = $this->arguments['prependOptionLabel'];
+            $output .= $this->renderOptionTag((string)$value, (string)$label, false) . LF;
+        }
+        return $output;
+    }
+
+    /**
+     * @param array<string, mixed> $arguments
+     */
+    protected function registerArguments(array $arguments): void
+    {
+        foreach ($arguments as $argumentName => $argumentConfiguration) {
+            $this->registerArgument(
+                $argumentName,
+                $argumentConfiguration['type'],
+                $argumentConfiguration['description'],
+                $argumentConfiguration['required'] ?? false,
+                $argumentConfiguration['defaultValue'] ?? null,
+                $argumentConfiguration['escape'] ?? null
+            );
+        }
+    }
+
+    protected function getSelectedValue(): string
+    {
+        $this->setRespectSubmittedDataValue(true);
+        return $this->getValueAttribute();
+    }
+
+    protected function isSelected(string $value): bool
+    {
+        $selectedValue = $this->getSelectedValue();
+
+        if ($value === $selectedValue || (string)$value === $selectedValue) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array<int, mixed> $options
+     */
+    protected function renderOptionTags($options): string
+    {
+        $output = '';
+        foreach ($options as $option) {
+            $output .= '<option value="' . $option['value'] . '"';
+            if ($option['isSelected']) {
+                $output .= ' selected="selected"';
+            }
+            $output .= '>' . htmlspecialchars((string)$option['label']) . '</option>' . LF;
+        }
+        return $output;
+    }
+
+    protected function renderOptionTag(string $value, string $label, bool $isSelected): string
+    {
+        $output = '<option value="' . htmlspecialchars((string)$value) . '"';
+        if ($isSelected) {
+            $output .= ' selected="selected"';
+        }
+        $output .= '>' . htmlspecialchars((string)$label) . '</option>';
+        return $output;
+    }
+
+    /**
+     * @return array<int, mixed>
+     */
+    protected function getOptions()
+    {
+        return [];
     }
 }
