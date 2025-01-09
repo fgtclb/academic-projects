f9a9ea819bc93512c580453b8eeefab214622821
Philipp Halle <p.halle@web-vision.de>
2025-01-09 09:40:38 +0100

    [TASK] Optimze icon handling

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,15 +3,15 @@
 declare(strict_types=1);
 
 use FGTCLB\AcademicProjects\Enumeration\CategoryTypes;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-$sourceString = function (string $icon) {
-    return sprintf(
-        'EXT:academic_projects/Resources/Public/Icons/CategoryTypes/%s.svg',
-        \TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($icon)
-    );
-};
+$sourceString = static fn (string $icon): string => sprintf(
+    'EXT:academic_projects/Resources/Public/Icons/CategoryTypes/%s.svg',
+    GeneralUtility::underscoredToUpperCamelCase($icon)
+);
 
-$identifierString = function (string $identifier) {
+$identifierString = static function (string $identifier) {
     return sprintf(
         'academic-project-%s',
         $identifier
@@ -20,19 +20,19 @@ $identifierString = function (string $identifier) {
 
 return [
     $identifierString(CategoryTypes::TYPE_COMPETENCE_FIELD) => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => $sourceString(CategoryTypes::TYPE_COMPETENCE_FIELD),
     ],
     $identifierString(CategoryTypes::TYPE_COOPERATION) => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => $sourceString(CategoryTypes::TYPE_COOPERATION),
     ],
     $identifierString(CategoryTypes::TYPE_DEPARTMENT) => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => $sourceString(CategoryTypes::TYPE_DEPARTMENT),
     ],
     $identifierString(CategoryTypes::TYPE_FUNDING_PARTNER) => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => $sourceString(CategoryTypes::TYPE_FUNDING_PARTNER),
     ],
 ];
