1745030105097f88b30ab8018a43908d7074f29c
Stefan Bürk <stefan@buerk.tech>
2025-03-19 22:07:24 +0100

    [TASK] Harmonize version constraints
    To prepare towards adding a release strategy based
    on TYPO3 Core workflow, it is important to have an
    clean starting point and this change is targeting.
    
    Following things are done:
    
    * All `ext_emconf.php` version of all extensions
      including fixture extensions are set to 1.1.5,
      a version higher than the highest released for
      an invidial extension albeit not beeing there,
      and allowing to switch to the release version
      creating the release commit and having changes
      ensured in extensions. Due to missing tooling,
      this is done by hand for now.
    * Add all academic extensions to path repository
      in the root `composer.json` declaring version
      `1.2.0-dev`. Due to missing tooling, this is
      done by hand for now as jq did not payed out
      in a timely manner. The same is done for the
      `packages-dev` in the same way.
    * Adjust `composer.json` requires on academic
      extensions to `1.2.*@dev` using composer
      commands.
    
    Note that this setup allows us to ommit using
    `COMPOSER_ROOT_VERSION='1.2.x-dev'` or similar,
    albeit still set within dispatched commands in
    the `Build/Scripts/runTests.sh` script.
    
    Used command(s):
    
    ```shell
    COMPOSER_BIN="$( which composer2-81 )" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages-dev/monorepo-shared \
      "fgtclb/academic-bite-jobs":"1.2.0-dev" \
      "fgtclb/academic-contacts4pages":"1.2.0-dev" \
      "fgtclb/academic-jobs":"1.2.0-dev" \
      "fgtclb/academic-partners":"1.2.0-dev" \
      "fgtclb/academic-persons":"1.2.0-dev" \
      "fgtclb/academic-persons-edit":"1.2.0-dev" \
      "fgtclb/academic-persons-sync":"1.2.0-dev" \
      "fgtclb/academic-programs":"1.2.0-dev" \
      "fgtclb/academic-projects":"1.2.0-dev" \
      "fgtclb/category-types":"1.2.0-dev" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages/fgtclb/academic-contact4pages \
      "fgtclb/academic-persons":"1.2.*@dev" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages/fgtclb/academic-partners \
      "fgtclb/category-types":"1.2.*@dev" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages/fgtclb/academic-persons-edit \
      "fgtclb/academic-persons":"1.2.*@dev" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages/fgtclb/academic-persons-sync\
      "fgtclb/academic-persons":"1.2.*@dev" \
      "fgtclb/academic-persons-edit":"1.2.*@dev" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages/fgtclb/academic-programs \
      "fgtclb/category-types":"1.2.*@dev" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages/fgtclb/academic-programs \
      "fgtclb/category-types":"1.2.*@dev" \
    && ${COMPOSER_BIN} require --no-update \
      -d packages/fgtclb/academic-persons/Tests/Functional/Fixtures/Extensions/test_plugin_templates \
      "fgtclb/academic-persons":"1.2.*@dev" \
      "typo3/cms-core":"^11.5 || ^12.4"
    ```

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Academic project pages for TYPO3 with specific structured data and typed sys_categories',
     'category' => 'fe',
     'state' => 'beta',
-    'version' => '1.1.0',
+    'version' => '1.1.5',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
             'extbase' => '11.5.0-11.5.99',
             'fluid' => '11.5.0-11.5.99',
             'install' => '11.5.0-11.5.99',
-            'category_types' => '1.0.0-1.99.99',
+            'category_types' => '1.1.5',
         ],
         'conflicts' => [],
         'suggests' => [],
